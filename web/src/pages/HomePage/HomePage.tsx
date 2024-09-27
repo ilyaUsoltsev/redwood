eb0a29d53b654c1e37fa79c085c1d658089d0233
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2024-09-27 14:54:32 +0200

    Article cell

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,16 @@
 // import { Link, routes } from '@redwoodjs/router'
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
-      <p>Home page</p>
+      <p><ArticlesCell /></p>
+
+
     </>
   )
 }
