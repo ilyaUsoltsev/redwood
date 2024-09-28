49bf2d415e7581c274303522f3e9bd5ec6263756
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2024-09-28 11:11:39 +0200

    Contact form

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -2,22 +2,46 @@
 import {
   FieldError,
   Form,
+  FormError,
   Label,
   Submit,
   TextAreaField,
   TextField,
+  useForm,
 } from '@redwoodjs/forms'
-import { Metadata } from '@redwoodjs/web'
+import { Metadata, useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast';
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+
+  const [create, {loading, error}] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Message sent!');
+      formMethods.reset();
+    },
+  });
+
+  const formMethods = useForm({ mode: 'onBlur' });
+
   const onSubmit = (data) => {
-    console.log(data)
+    console.log(data);
+    create({ variables: { input: data } });
   }
   return (
     <>
       <Metadata title="Contact" description="Contact page" />
+      <Toaster />
 
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} formMethods={formMethods} error={error}>
+        <FormError error={error} wrapperClassName='text-red-500' />
         <Label  name='name'>Name</Label>
         <TextField
           name="name"
@@ -25,26 +49,26 @@ const ContactPage = () => {
           validation={{ required: true }}
           errorClassName="error"
         />
-        <FieldError name="name" className="error" />
+        <FieldError name="name" className="text-red-500" />
         <br />
         <Label name='email' >Email</Label>
         <TextField
           name="email"
           placeholder="Your email"
           errorClassName="error"
-          validation={{ required: true }}
+          validation={{ required: true, pattern: /[^@]+@[^.]+\..+/ }}
         />
-        <FieldError name="email" className="error" />
+        <FieldError name="email" className="text-red-500" />
         <br />
-        <Label name='message' >Message</Label>
+        <Label name='message'  >Message</Label>
         <TextAreaField
           name="message"
           placeholder="Your message"
           errorClassName="error"
           validation={{ required: true }}
         />
-        <FieldError name="message" className="rw-label-error rw-label" />
-        <Submit>Send</Submit>
+        <FieldError name="message" className="text-red-500" />
+        <Submit disabled={loading}>Send</Submit>
       </Form>
     </>
   )
