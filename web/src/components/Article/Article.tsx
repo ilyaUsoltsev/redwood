18e59c6bdffdef7d0148815742b93a9fce1275f3
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2024-09-29 20:53:18 +0200

    Comments and migrations

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -1,4 +1,5 @@
 import { Link, routes } from '@redwoodjs/router'
+import CommentsCell from 'src/components/CommentsCell'
 
 import type { Post } from 'types/graphql'
 
@@ -13,7 +14,7 @@ interface Props {
 
 const Article = ({ article, summary = false }: Props) => {
   return (
-    <article className="mt-10">
+    <article>
       <header>
         <h2 className="text-xl text-blue-700 font-semibold">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
@@ -22,6 +23,11 @@ const Article = ({ article, summary = false }: Props) => {
       <div className="mt-2 text-gray-900 font-light">
         {summary ? truncate(article.body, 100) : article.body}
       </div>
+      {!summary && (
+        <div className="mt-12">
+          <CommentsCell />
+        </div>
+      )}
     </article>
   )
 }
